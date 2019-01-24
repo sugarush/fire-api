23eb0655e57b339b47aee576f51007b4ea41a9d9
Lucifer Avada <lucifer.avada@gmail.com>
2019-01-24 18:48:34 -0500

    update example code

--- models.py
+++ models.py
@@ -3,11 +3,22 @@ from sugar_api import JSONAPIMixin
 
 
 class AccountType(MongoDBModel, JSONAPIMixin):
+
+    __acl__ = { 'administrator': ['all'] }
+
     name = Field(required=True)
-    attributes = Field(type=dict, required=True)
+    attributes = Field(type=dict)
 
 
 class User(MongoDBModel, JSONAPIMixin):
+
+    __acl__ = {
+        'self': ['read', 'read_all', 'update'],
+        'administrator': ['all'],
+        'other': ['read', 'read_all'],
+        'unauthorized': ['create']
+    }
+
     username = Field(required=True)
     password = Field(required=True)
 
