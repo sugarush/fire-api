958b117402f3dc4f81f0d498e6d2fc9804477072
Lucifer Avada <lucifer.avada@gmail.com>
2019-01-25 10:55:30 -0500

    update k6 script

--- k6.js
+++ k6.js
@@ -6,40 +6,20 @@ let username = 'administrator';
 let password = 'administrator';
 
 export default function() {
-    // Login to get API session token
-    let res = http.post('http://localhost:8080/v1/authentication',
-        JSON.stringify({ data: { username: username, password: password } }),
-        { headers: {
-          'Content-Type': 'application/vnd.api+json',
-          'Accept': 'application/vnd.api+json'
-        } }
-    );
-
-    let token = res.json().data.token;
-
-    /*res = http.get('http://localhost:8080/v1/users/' + token.id, {
+    let res = http.get('http://localhost:8080/v1/users', {
       headers: {
         'Content-Type': 'application/vnd.api+json',
         'Accept': 'application/vnd.api+json'
       }
     });
 
-    console.log(res.json().data)
-
-    sleep(1);
-
-    // Extract API session token from JSON response
-    let apiToken = res.json().token.key;
-
-    // Use session token to make API requests
-    res = http.get('https://api.loadimpact.com/v3/account/me',
-        { headers: { 'Authorization': 'Token ' + apiToken } }
-    );
     check(res, {
-        'status is 200': (res) => res.status === 200,
-        'content-type is application/json': (res) => res.headers['Content-Type'] === 'application/json',
-        'content OK': (res) => JSON.parse(res.body).hasOwnProperty('organizations')
+      'status is 200': (res) => res.status === 200,
+      'content-type is application/vnd.api+json': (res) => {
+        res.headers['Content-Type'] === 'application/vnd.api+json';
+      },
+      'content ok': (res) => {
+        JSON.parse(res.body).hasOwnProperty('data');
+      }
     });
-
-    sleep(3);*/
 }
