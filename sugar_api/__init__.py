cfbce3a5517b59a76d81393d895b99bb25061f02
Lucifer Avada <lucifer.avada@gmail.com>
2019-06-06 07:49:17 -0700

    rename jsonapi to mixin

--- sugar_api/__init__.py
+++ sugar_api/__init__.py
@@ -2,6 +2,6 @@ from . acl import acl
 from . cors import CORS
 from . error import Error
 from . header import accept, content_type, jsonapi
-from . jsonapi import JSONAPIMixin
+from . mixin import JSONAPIMixin
 from . scope import scope
 from . webtoken import WebToken, webtoken
