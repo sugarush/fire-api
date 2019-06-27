e7f98569ba560ac730c28a0439bbffe21cc1b608
Lucifer Avada <lucifer.avada@gmail.com>
2019-06-27 00:24:09 -0400

    import Redis

--- sugar_api/__init__.py
+++ sugar_api/__init__.py
@@ -4,6 +4,7 @@ from . error import Error
 from . header import accept, content_type, jsonapi
 from . mixin import JSONAPIMixin
 from . rate import rate
+from . redis import Redis
 from . scope import scope
 from . validate import validate
 from . webtoken import WebToken, webtoken
