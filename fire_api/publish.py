00329577568720e1b55c436d2751b43b4f922628
Paul Severance <paul.severance@gmail.com>
2020-01-25 13:50:33 -0500

    remove publish.py

--- fire_api/publish.py
+++
@@ -1,6 +0,0 @@
-def publish(handler):
-    async def decorator(request, *args, **kargs):
-        response = await handler(request, *args, **kargs)
-        print(dir(response))
-        return response
-    return decorator
