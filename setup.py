3cba3d2b5222c49d1cef1ca45719347d067dc804
Paul Severance <paul@sugarush.io>
2020-02-06 16:46:16 -0500

    update depencencies

--- setup.py
+++ setup.py
@@ -13,15 +13,9 @@ setup(
     ],
     description='A JSONAPI implementation.',
     install_requires=[
-        'fire_document',
-        'fire_router',
-        'fire_asynctest',
-        'fire_odm'
-    ],
-    dependency_links=[
-        'git+https://github.com/sugarush/fire-document@master#egg=fire-document',
-        'git+https://github.com/sugarush/fire-router@master#egg=fire-router',
-        'git+https://github.com/sugarush/fire-asynctest@master#egg=fire-asynctest',
-        'git+https://github.com/sugarush/fire-odm@master#egg=fire-odm',
+        'fire_document@git+https://github.com/sugarush/fire-document@master#egg=fire-document',
+        'fire_router@git+https://github.com/sugarush/fire-router@master#egg=fire-router',
+        'fire_asynctest@git+https://github.com/sugarush/fire-asynctest@master#egg=fire-asynctest',
+        'fire_odm@git+https://github.com/sugarush/fire-odm@master#egg=fire-odm'
     ]
 )
